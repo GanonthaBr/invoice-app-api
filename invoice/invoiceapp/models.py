f7a818b63e2c67293df4887603e2aaf547cc0816
Ganontha <bganontha@gmail.com>
2025-01-23 14:21:11 +0100

    Update invoice API endpoint and modify Invoice model for tax handling

--- invoice/invoiceapp/models.py
+++ invoice/invoiceapp/models.py
@@ -32,12 +32,19 @@ class Client(models.Model):
     client_mail = models.EmailField(max_length=100)
    
 class Invoice(models.Model):
+    #choices, tuple
+    TYPE_TAX = (
+        ('TVA', 'TVA'),
+        ('ISB','ISB')
+    )
+
+
     topic = models.TextField()
     number = models.CharField(max_length=100)
     echeance = models.DateField()
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    tax = models.IntegerField()
-    type_tax = models.CharField(max_length=100)
+    tax = models.BooleanField()
+    type_tax = models.CharField(choices=TYPE_TAX,max_length=100)
     payment_mode = models.TextField()
 
     def save(self,*args, **kwargs):
@@ -52,8 +59,14 @@ class Invoice(models.Model):
     @property
     def total_amount(self):
         total = sum(designation.designation_price for designation in self.designations.all())
-        total_with_tax = total + (total * self.tax/100)
-        return total_with_tax
+        # if tax = True, check if type_tax is 'isb' or 'vat'. if 'isb' substract 2% if 'vat' add 16% to total. In case tax = True, nothing is touched
+        if self.tax:
+            if self.type_tax == 'isb' or self.type_tax == 'ISB':
+                total -= total * 0.02
+            elif self.type_tax == 'tva' or self.type_tax == 'TVA':
+                total += total * 0.16
+        
+        return total
     
 class Designation(models.Model):
     invoice = models.ForeignKey(Invoice,related_name='designations',on_delete=models.CASCADE)
@@ -64,4 +77,4 @@ class Designation(models.Model):
 
     @property
     def designation_price(self):
-        return self.designation_unit_price * self.designation_quantity
+        return float(self.designation_unit_price * self.designation_quantity)
